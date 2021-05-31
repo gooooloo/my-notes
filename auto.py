48e0c003063d5f059dbcbaf6a85ef2bcd78451f7
gooooloo <qidu.lin@gmail.com>
2021-06-01 00:35:25 +0800

    update

--- auto.py
+++ auto.py
@@ -1,6 +1,37 @@
 import glob
+import os
+import time
+import shutil
+from os.path import basename, join, dirname, abspath, isdir, sep
 from urllib import parse
 from os.path import basename, join, dirname, abspath, isdir, sep
+
+
+def docmd(cmd):
+    print(cmd)
+    os.system(cmd)
+
+
+def auto_git_add():
+    for fn in glob.glob(join('*', '**', '.gitignore'), recursive=True):
+        os.remove(fn)
+
+    for fn in glob.glob(join('*', '**', '*.md'), recursive=True):
+        with open(fn, mode="r") as f:
+            firstline = f.readline()
+
+        if firstline == '#git\n':
+            fn2 = fn.replace(' ', '\ ') 
+            cmd = f"git add {fn2}"
+            docmd(cmd)
+        else:
+            xx = join(dirname(fn), '.gitignore')
+            fn2 = basename(fn) + '\n'
+            with open(xx, 'a') as f:
+                print(fn2, file=f)
+
+    for fn in glob.glob(join('*', '**', '.gitignore'), recursive=True):
+        docmd(f"git add {fn}")
 
 
 def urlencode(s):
@@ -8,7 +39,7 @@ def urlencode(s):
     return ret
 
 
-def main():
+def update_sidebar():
     md_list = [abspath(fn) for fn in glob.glob(join('**', '*.md'), recursive=True) if not basename(fn).startswith('_') and basename(fn) != 'README.md']
 
     root_abs = abspath('.')
@@ -43,7 +74,10 @@ def main():
             else:
                 x += line[-1]
             print(x, file=f)
+    
+    docmd('git add _sidebar.md')
 
 
 if __name__ == '__main__':
-    main()
+    auto_git_add()
+    update_sidebar()
