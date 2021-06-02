f1853babb6419341b8c45793821bad1393477ddb
gooooloo <qidu.lin@gmail.com>
2021-06-02 11:01:32 +0800

    handle imgs

--- auto.py
+++ auto.py
@@ -5,6 +5,7 @@ import shutil
 from os.path import basename, join, dirname, abspath, isdir, sep
 from urllib import parse
 from os.path import basename, join, dirname, abspath, isdir, sep
+import re
 
 
 def docmd(cmd):
@@ -25,7 +26,7 @@ def auto_git_add():
 
     for fn in glob.glob(join('*', '**', '*.md'), recursive=True):
         if ingit(fn):
-            fn2 = fn.replace(' ', '\ ') 
+            fn2 = fn.replace(' ', '\ ')
             cmd = f"git add {fn2}"
             docmd(cmd)
         else:
@@ -62,7 +63,7 @@ def update_sidebar():
             if y != ('',) and y not in extended_set:
                 extended_set.add(y)
                 extended_list.append(y)
-    
+
     with open("_sidebar.md", "w") as f:
         print('- [首页](/)', file=f)
         for line in extended_list:
@@ -78,10 +79,44 @@ def update_sidebar():
             else:
                 x += line[-1]
             print(x, file=f)
-    
+
     docmd('git add _sidebar.md')
+
+
+def update_img():
+    img_list = set()
+    md_list = [abspath(fn) for fn in glob.glob(join('**', '*.md'), recursive=True) if not basename(fn).startswith('_') and basename(fn) != 'README.md' and ingit(fn)]
+    for md in md_list:
+        with open(md, 'r') as f:
+            lines = f.readlines()
+        for postfix in ('png', 'jpg', 'svg'):
+            sublines = [l for l in lines if f'.{postfix}' in l]
+            for line in sublines:
+                p = f'\(.*{postfix}\)'
+                x = re.search(p, line)
+                x= x.group()[1:-1]
+                x = join(dirname(md), x)
+                img_list.add(abspath(x))
+
+    for x in img_list:
+        print(x)
+
+    all_img_list = set()
+    for postfix in ('png', 'jpg', 'svg'):
+        for fn in glob.glob(join('**', f'*.{postfix}')):
+            x = abspath(fn)
+            if x in img_list:
+                x = x.replace(' ', '\ ')
+                docmd(f'git add {x}')
+            else:
+                gi = join(dirname(fn), '.gitignore')
+                with open(gi, 'a') as f:
+                    print(f'gitignore: {x}')
+                    print(basename(x), file=f)
+                docmd(f'git add {gi}')
 
 
 if __name__ == '__main__':
     auto_git_add()
     update_sidebar()
+    update_img()
